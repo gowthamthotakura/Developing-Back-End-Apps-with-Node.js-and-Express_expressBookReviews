976218400bfb5638eadfc5fabb550c05cf04541d
gowthamthotakura <gowthamthotakura@gmail.com>
2024-09-02 09:19:22 -0400

    updating the code in progress

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,20 +1,50 @@
+const Axios = require("axios")
 const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-
+ //Write your code below
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+ 
+  const username = req.body.username;
+  const password = req.body.password;
+  if(username&&password){
+      const present = users.filter((user)=> user.username === username)
+      if(present.length===0){
+          users.push({"username":req.body.username,"password":req.body.password});
+          return res.status(201).json({message:"USer Created successfully"})
+      }
+      else{
+        return res.status(400).json({message:"Already exists"})
+      }
+  }
+  else if(!username && !password){
+    return res.status(400).json({message:"Bad request"})
+  }
+  else if(!username || !password){
+    return res.status(400).json({message:"Check username and password"})
+  }  
+
+   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+public_users.get('/', (req, res) => {
+    const getBooks = () => {
+        return new Promise((resolve,reject) => {
+          setTimeout(() => {
+            resolve(books);
+            ),1000);
+        })
+    }
+    getBooks().then((books) => {
+        res.json(books);
+    }).catch((err) =>{
+      res.status(500).json({error: "An error occured"});
+    });
+      
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
